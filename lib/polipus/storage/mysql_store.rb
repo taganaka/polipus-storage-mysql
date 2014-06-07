de60f2b98604fb9857a6b527d97c8cbd4470fd7b
Francesco Laurita <taganaka@gmail.com>
2014-06-07 16:52:43 -0700

    code style applied

--- lib/polipus/storage/mysql_store.rb
+++ lib/polipus/storage/mysql_store.rb
@@ -45,59 +45,61 @@ module Polipus
       def clear
         @my.query("DELETE FROM #{@tbl}")
       end
+
       private
-        def setup
-          create_table = %Q(
-            CREATE TABLE IF NOT EXISTS #{@tbl} (
-              uuid          varchar(32) PRIMARY KEY,
-              url           varchar(255),
-              headers       blob,
-              body          blob,
-              links         blob,
-              code          int,
-              depth         int,
-              referer       varchar(255),
-              redirect_to   varchar(255),
-              response_time int,
-              fetched       boolean,
-              user_data     blob,
-              fetched_at    int,
-              error         varchar(255)
+
+      def setup
+        create_table = %Q(
+          CREATE TABLE IF NOT EXISTS #{@tbl} (
+            uuid          varchar(32) PRIMARY KEY,
+            url           varchar(255),
+            headers       blob,
+            body          blob,
+            links         blob,
+            code          int,
+            depth         int,
+            referer       varchar(255),
+            redirect_to   varchar(255),
+            response_time int,
+            fetched       boolean,
+            user_data     blob,
+            fetched_at    int,
+            error         varchar(255)
+          )
+        )
+        @my.query(create_table)
+      end
+
+      def page_to_sql(page)
+        %Q(
+          INSERT INTO #{@tbl}
+            VALUES (
+              '#{uuid(page)}',
+              '#{@my.escape(page.url.to_s)}',
+              '#{@my.escape(Marshal.dump(page.headers))}',
+              '#{@my.escape(page.body)}',
+              '#{@my.escape(Marshal.dump(page.links))}',
+              #{page.code.to_i},
+              #{page.depth.to_i},
+              '#{@my.escape(page.referer.to_s)}',
+              '#{@my.escape(page.redirect_to.to_s)}',
+              #{page.response_time.to_i},
+              #{page.fetched?},
+              '#{@my.escape(Marshal.dump(page.user_data))}',
+              #{page.fetched_at.to_i},
+              '#{@my.escape(page.error.to_s)}'
             )
-          )
-          @my.query(create_table)
-        end
+            ON DUPLICATE KEY UPDATE
+              fetched_at = UNIX_TIMESTAMP()
+        )
+      end
 
-        def page_to_sql(page)
-          %Q(
-            INSERT INTO #{@tbl}
-              VALUES (
-                '#{uuid(page)}',
-                '#{@my.escape(page.url.to_s)}',
-                '#{@my.escape(Marshal.dump(page.headers))}',
-                '#{@my.escape(page.body)}',
-                '#{@my.escape(Marshal.dump(page.links))}',
-                #{page.code.to_i},
-                #{page.depth.to_i},
-                '#{@my.escape(page.referer.to_s)}',
-                '#{@my.escape(page.redirect_to.to_s)}',
-                #{page.response_time.to_i},
-                #{page.fetched?},
-                '#{@my.escape(Marshal.dump(page.user_data))}',
-                #{page.fetched_at.to_i},
-                '#{@my.escape(page.error.to_s)}'
-              )
-              ON DUPLICATE KEY UPDATE
-                fetched_at = UNIX_TIMESTAMP()
-          )
+      def load_page(hash)
+        %w(links user_data).each do |f|
+          hash[f] = Marshal.load(hash[f]) unless hash[f].nil?
         end
-
-        def load_page(hash)
-          %w(links user_data).each do |f|
-            hash[f] = Marshal.load(hash[f]) unless hash[f].nil?
-          end
-          Page.from_hash(hash)
-        end
+        Page.from_hash(hash)
+      end
     end
   end
 end
